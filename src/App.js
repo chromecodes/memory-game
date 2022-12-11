ad47a93c374637ac17113745a188092e65f669ad
chromecodes <hameedhussain782@gmail.com>
2022-12-11 23:25:07 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Game from "./components/Game";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-   
+    <div className='app'>
+      <Header />
+      <Game />
+      <Footer />
+    </div>
   );
 }
 
