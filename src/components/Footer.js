9b8dc55e6cdfa81c071eb23e3f9cc5c068ab3e15
chromecodes <hameedhussain782@gmail.com>
2022-12-15 23:05:55 +0530

    added file

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { FaGithub,FaCode } from 'react-icons/fa';
+export default class Footer extends Component {
+  render() {
+    return (
+         <footer className='footer' id="footer">
+                <span className="user" >
+                    The <a href="https://www.theodinproject.com/" className="odin">Odin</a> Project 
+                    <a href="https://github.com/chromecodes/cv-project" > <FaCode className="faCode" /> </a>
+                     chromecodes 
+                    <a href="https://github.com/chromecodes" > <FaGithub className="faGithub" /></a>
+                </span>
+            </footer>
+    )
+  }
+}
