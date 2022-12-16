e79ec013ee394ce0b3ad92af23b9b4c056c2e935
chromecodes <hameedhussain782@gmail.com>
2022-12-16 23:14:14 +0530

    updated file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,42 @@ import Game from "./components/Game";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+const Pokedex = require("pokeapi-js-wrapper");
+
+const P = new Pokedex.Pokedex();
+
+P.getPokemonByName("eevee").then(function (response) {
+  console.log(response);
+});
+
+// const customOptions = {
+//   protocol: "https",
+//   hostName: "localhost:443",
+//   versionPath: "/api/v2/",
+//   cache: true,
+//   timeout: 5 * 1000, // 5s
+//   cacheImages: true,
+// };
+// const P = new Pokedex.Pokedex(customOptions);
+
+// P.getPokemonByName("eevee").then(function (response) {
+//   console.log(response);
+// });
+
+// P.getPokemonSpeciesByName(25).then(function (response) {
+//   console.log(response);
+// });
+
+// P.getBerryByName(["cheri", "chesto", 5]).then(function (response) {
+//   // `response` will be an Array containing 3 Objects
+//   // response.forEach((item) => {console.log(item.size)}) // 80,50,20
+//   console.log(response);
+// });
+
+// P.getMachineById(3).then(function (response) {
+//   console.log(response);
+// });
+
 function App() {
   return (
     <div className='app'>
