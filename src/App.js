67282619472294a1670ed8f60f7510efa9121288
chromecodes <hameedhussain782@gmail.com>
2022-12-25 23:30:42 +0530

    updated file

--- src/App.js
+++ src/App.js
@@ -11,42 +11,36 @@ async function getData() {
 }
 
 function App() {
-  const [data, setData] = useState(getData);
+  const [data, setData] = useState(1);
 
   const resetData = () => {
     setData(getData);
   };
 
-  useEffect(()=>{
-    import { useEffect, useState } from "react"
+  //   useEffect(()=>{
 
-export default function Test() {
+  //   const [data, setData] = useState('')
 
-  const [data, setData] = useState('')
+  //   useEffect(() => {
 
+  //     const callData = async () => {
+  //       const data = await fetch('https://jsonplaceholder.typicode.com/todos/10').then(data => data.json())
+  //       setData(data)
 
-  useEffect(() => {
+  //     }
 
-    const callData = async () => {
-      const data = await fetch('https://jsonplaceholder.typicode.com/todos/10').then(data => data.json())
-      setData(data)
+  //     callData()
+  //   }, [])
 
-    }
+  //   return (data === '' ? <>loading ... </> : <div>{data.title}</div>)
 
-
-    callData()
-  }, [])
-
-  return (data === '' ? <>loading ... </> : <div>{data.title}</div>)
-
-}
-  },[])
-
+  // }
+  //   },[])
 
   return (
     <div className='app'>
       <Header />
-      <Game data={data === [] ? 1 : data} />
+      <Game data={data} />
       <button onClick={resetData}>RESET</button>
       <Footer />
     </div>
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "./Card";
 
-export default function Game() {
-  const { data } = this.props;
+export default function Game(props) {
+  console.log(props.data);
+
+  const { data } = props;
+
+  console.log(data);
+  const createCard = () => {
+    if (data === data[0]) {
+      data.forEach((datum) => {
+        <Card data={datum} />;
+      });
+    }
+  };
+
+  useEffect(() => {
+    createCard();
+  }, [data]);
   return (
     <>
-      <div className='main'>
-        {data.forEach((datum) => {
-          <Card data={datum} />;
-        })}
-      </div>
-      ;
+      <div className='main'>{createCard()}</div>
     </>
   );
 }
