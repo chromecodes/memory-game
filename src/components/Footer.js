3457b4e21fd993ea8048222c5f6b63eb7ba3a8bf
chromecodes <hameedhussain782@gmail.com>
2023-01-01 17:37:11 +0530

    updated file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
-import { FaGithub,FaCode } from 'react-icons/fa';
+import React, { Component } from "react";
+import { FaGithub, FaCode } from "react-icons/fa";
 export default class Footer extends Component {
   render() {
     return (
-         <footer className='footer' id="footer">
-                <span className="user" >
-                    The <a href="https://www.theodinproject.com/" className="odin">Odin</a> Project 
-                    <a href="https://github.com/chromecodes/cv-project" > <FaCode className="faCode" /> </a>
-                     chromecodes 
-                    <a href="https://github.com/chromecodes" > <FaGithub className="faGithub" /></a>
-                </span>
-            </footer>
-    )
+      <footer className='footer' id='footer'>
+        <span className='user'>
+          The{" "}
+          <a href='https://www.theodinproject.com/' className='odin'>
+            Odin
+          </a>{" "}
+          Project
+          <a href='https://github.com/chromecodes/memory-game'>
+            {" "}
+            <FaCode className='faCode' />{" "}
+          </a>
+          chromecodes
+          <a href='https://github.com/chromecodes'>
+            {" "}
+            <FaGithub className='faGithub' />
+          </a>
+        </span>
+      </footer>
+    );
   }
 }
